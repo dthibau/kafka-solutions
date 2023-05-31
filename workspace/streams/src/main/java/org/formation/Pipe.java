8de0b891deea505c71e936e95b5b98442a525e7d
David THIBAU <david.thibau@gmail.com>
2023-05-31 10:55:51 +0200

    External Bitnami

--- workspace/streams/src/main/java/org/formation/Pipe.java
+++ workspace/streams/src/main/java/org/formation/Pipe.java
@@ -42,7 +42,7 @@ public class Pipe {
     	
         Properties props = new Properties();
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-pipe");
-        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:19092,localhost:19093");
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, CourierSerde.class);
 
