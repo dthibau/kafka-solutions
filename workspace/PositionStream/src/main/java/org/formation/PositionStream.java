80b15a02ddad193105987ee6920263977a519b0d
David THIBAU <david.thibau@gmail.com>
2025-05-25 19:40:00 +0200

    Agregate

--- workspace/PositionStream/src/main/java/org/formation/PositionStream.java
+++ workspace/PositionStream/src/main/java/org/formation/PositionStream.java
@@ -4,16 +4,17 @@ import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig;
 import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig;
 import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.utils.Bytes;
 import org.apache.kafka.streams.*;
 import org.apache.kafka.streams.kstream.*;
+import org.apache.kafka.streams.state.KeyValueStore;
 import org.formation.model.Courier;
 import org.formation.model.Position;
 
 import java.time.Duration;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
+import java.util.*;
 import java.util.concurrent.CountDownLatch;
 
 public class PositionStream {
@@ -22,7 +23,7 @@ public class PositionStream {
     public static void main(String[] args) {
 
         Properties props = new Properties();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "windowed-count-streams-position-branch");
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "list-streams-position");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:19092");
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, SpecificAvroSerde.class);
@@ -39,38 +40,55 @@ public class PositionStream {
         SpecificAvroSerde<Courier> coursierSerde = new SpecificAvroSerde<>();
         coursierSerde.configure(config, true);
 
+        Serde<List<String>> listSerde = Serdes.ListSerde(ArrayList.class, Serdes.String());
+
 // Création d’une topolgie de processeurs
         final StreamsBuilder builder = new StreamsBuilder();
-        Map<String, KStream<Position, String>> branches = builder.<String, Courier>stream("avro-position")
+
+        // Arrondir les Positions
+        KStream<String,Position> coursierStream = builder.<String, Courier>stream("avro-position")
                 .mapValues(coursier -> {
                     Position position = (Position)coursier.getPosition();
                     position.setLatitude((double)Math.round(position.getLatitude()));
                     position.setLongitude((double)Math.round(position.getLongitude()));
-                    return coursier;
-                })
-                .selectKey((k, coursier) -> (Position)coursier.getPosition())
-                .mapValues(courier -> courier.getId().toString())
-                .split(Named.as("Branch-"))
-                .branch((key, value) -> key.getLatitude() == 45,  /* first predicate  */
-                        Branched.as("South"))
-                .defaultBranch(Branched.as("North"));
+                    return position;
+                });
 
-                branches.get("Branch-South")
-                        .groupByKey(Grouped.with(positionSerde, Serdes.String()))
-                        .windowedBy(TimeWindows.ofSizeWithNoGrace(Duration.ofMinutes(1)))
-                        .count(Materialized.with(positionSerde, Serdes.Long())).toStream()
-                        .map((windowedPosition,count) -> {
-                            return new KeyValue<Position,String>(windowedPosition.key(),count.toString());
-                        })
-                        .to("windowed-count-avro-position-south", Produced.with(positionSerde, Serdes.String()));
-                branches.get("Branch-North")
-                        .groupByKey(Grouped.with(positionSerde, Serdes.String()))
-                        .windowedBy(TimeWindows.ofSizeWithNoGrace(Duration.ofMinutes(1)))
-                        .count(Materialized.with(positionSerde, Serdes.Long())).toStream()
-                        .map((windowedPosition,count) -> {
-                            return new KeyValue<Position,String>(windowedPosition.key(),count.toString());
-                        })
-                        .to("windowed-count-avro-position-north", Produced.with(positionSerde, Serdes.String()));
+               // Stocker les dernières positions dans uen table
+                                KTable<String,Position> coursiersTable = coursierStream.toTable(
+                                Materialized.<String, Position, KeyValueStore<Bytes, byte[]>>as("coursiers-last-position")
+                                                .withKeySerde(Serdes.String())
+                                        .withValueSerde(positionSerde)
+                        );
+                        // Grouper (partitionner) par rapport à la Position
+                        KGroupedTable<Position, String> groupedTable = coursiersTable.groupBy((key, value) ->  KeyValue.pair(value, key), Grouped.with(positionSerde, Serdes.String()));
+
+                        // Agrégation avec adder et substractor
+                                KTable<Position,List<String>> listeCoursierParPosition = groupedTable
+                                 .aggregate(
+                                  () -> {
+                                          System.out.println("Initializing");
+                                          return new ArrayList<String>();
+                                      },
+                                    (position, newCoursier, currentList) -> {
+                                          System.out.println("Current list for position " + position + " is " + currentList + "adding " + newCoursier);
+                                          if ( !currentList.contains(newCoursier) ) {
+                                                  currentList.add(newCoursier);
+                                              }
+                                            return currentList;
+                                        },
+                                   (position, oldCoursier, currentList)  -> {
+                                            System.out.println("Current list for position " + position + " is " + currentList + "removing " + oldCoursier);
+                                            ArrayList<String> newList = new ArrayList<>(currentList);
+                                            newList.remove(oldCoursier);
+                                            return newList;
+                                        },
+                                        Materialized.<Position, List<String>, KeyValueStore<Bytes, byte[]>>as("aggregated-table-store") /* state store name */
+                                                        .withValueSerde(listSerde)
+                                                .withKeySerde(positionSerde)/* serde for aggregate value */
+                                );
+
+                        listeCoursierParPosition.toStream().mapValues(v -> v.toString()).to("coursiersParPosition",Produced.with(positionSerde,Serdes.String()));
 
         final Topology topology = builder.build();
 
