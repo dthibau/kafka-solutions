e3d2469306dfec61f86b40f2b0bbe360ce94a3d0
David THIBAU <david.thibau@gmail.com>
2025-05-25 18:54:46 +0200

    Window of one minute

--- workspace/PositionStream/src/main/java/org/formation/PositionStream.java
+++ workspace/PositionStream/src/main/java/org/formation/PositionStream.java
@@ -10,6 +10,7 @@ import org.apache.kafka.streams.kstream.*;
 import org.formation.model.Courier;
 import org.formation.model.Position;
 
+import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
@@ -21,10 +22,11 @@ public class PositionStream {
     public static void main(String[] args) {
 
         Properties props = new Properties();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-position-count");
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "windowed-count-streams-position-branch");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:19092");
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, SpecificAvroSerde.class);
+        props.put(StreamsConfig.NUM_STREAM_THREADS_CONFIG,5);
         props.put(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, REGISTRY_URL);
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
@@ -53,8 +55,22 @@ public class PositionStream {
                         Branched.as("South"))
                 .defaultBranch(Branched.as("North"));
 
-                branches.get("Branch-South").groupByKey(Grouped.with(positionSerde, Serdes.String())).count(Materialized.with(positionSerde, Serdes.Long())).toStream().mapValues(value -> value.toString()).to("count-avro-position-south", Produced.with(positionSerde, Serdes.String()));
-                branches.get("Branch-North").groupByKey(Grouped.with(positionSerde, Serdes.String())).count(Materialized.with(positionSerde, Serdes.Long())).toStream().mapValues(value -> value.toString()).to("count-avro-position-north", Produced.with(positionSerde, Serdes.String()));
+                branches.get("Branch-South")
+                        .groupByKey(Grouped.with(positionSerde, Serdes.String()))
+                        .windowedBy(TimeWindows.ofSizeWithNoGrace(Duration.ofMinutes(1)))
+                        .count(Materialized.with(positionSerde, Serdes.Long())).toStream()
+                        .map((windowedPosition,count) -> {
+                            return new KeyValue<Position,String>(windowedPosition.key(),count.toString());
+                        })
+                        .to("windowed-count-avro-position-south", Produced.with(positionSerde, Serdes.String()));
+                branches.get("Branch-North")
+                        .groupByKey(Grouped.with(positionSerde, Serdes.String()))
+                        .windowedBy(TimeWindows.ofSizeWithNoGrace(Duration.ofMinutes(1)))
+                        .count(Materialized.with(positionSerde, Serdes.Long())).toStream()
+                        .map((windowedPosition,count) -> {
+                            return new KeyValue<Position,String>(windowedPosition.key(),count.toString());
+                        })
+                        .to("windowed-count-avro-position-north", Produced.with(positionSerde, Serdes.String()));
 
         final Topology topology = builder.build();
 
